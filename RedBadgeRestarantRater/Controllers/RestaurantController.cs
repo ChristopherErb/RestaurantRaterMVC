19d552d7b0b8ed5245b8821ea5522a0462e38ba6
ChristopherErb01 <christophererb01@gmail.com>
2021-03-30 18:35:01 -0400

    Edit added

--- RedBadgeRestarantRater/Controllers/RestaurantController.cs
+++ RedBadgeRestarantRater/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RedBadgeRestarantRater.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -70,5 +71,47 @@ namespace RedBadgeRestarantRater.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        //GET Restaurant/Edit/{id}
+        //Get an id from user
+        //Handle if id is null
+        //Find a restaurant by id
+        //If restaurant doesn't exist
+        //return restaurant and view
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+
+        //post Restaurant/EDIT/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+
+        }
+
+
+
     }
 }
